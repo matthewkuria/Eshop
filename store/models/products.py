51f0fcde6fce56a339ecdee503a0739a4e308da1
matthewkuria <mathewkwachira@gmail.com>
2024-07-24 21:23:00 +0300

    Add product images and a product card

--- store/models/products.py
+++ store/models/products.py
@@ -10,6 +10,9 @@ class Products(models.Model):
 		max_length=250, default='', blank=True, null=True) 
 	image = models.ImageField(upload_to='uploads/products/') 
 
+	def __str__(self):
+		return self.name
+
 	@staticmethod
 	def get_products_by_id(ids): 
 		return Products.objects.filter(id__in=ids) 
