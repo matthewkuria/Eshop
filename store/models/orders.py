682a8f94205fb3c85dc41545abaa4d54b415066a
matthewkuria <mathewkwachira@gmail.com>
2024-07-29 09:04:49 +0300

    remove the slick styles

--- store/models/orders.py
+++ store/models/orders.py
@@ -1,23 +1,35 @@
-from django.db import models 
-from .products import Products 
-from .customer import Customer 
-import datetime 
-
+from django.db import models
+from .customer import Customer
+from .products import Products
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
-    transaction_id = models.CharField(max_length=100)
+    transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 class OrderItem(models.Model):
-    product = models.ForeignKey(Products, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=0)
+    product = models.ForeignKey(Products, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return str(self.product)
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
